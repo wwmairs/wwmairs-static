19ee4d025b25634d856ec4b974a5907b70229318
wwmairs <wwmairs@gmail.com>
2017-05-22 12:39:09 -0400

    panel colored as representation of sky color

--- bio.js
+++ bio.js
@@ -15,11 +15,8 @@ $.get('weather.json', function(data) {
     temp = forecast.currently.apparentTemperature;
     pressure = forecast.currently.pressure;
     clouds = forecast.currently.cloudCover;
-    background_color = 'rgba(0, 0, 255, ' + (1 - clouds) + ')';
+    background_color = 'rgba(51, 153, 255, ' + (1 - clouds) + ')';
     color = 'rgba(0, 0, 0, ' + clouds + ')';
-
-    console.log(background_color);
-    console.log(color);
 
     $('#weather').append(temp + ' degrees, and the barometric pressure is ' + pressure + ' milibars.');
     $('#where').css('background', background_color);
