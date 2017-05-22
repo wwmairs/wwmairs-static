cc3e24086147dd5b6c0eb7127e3d9f85997e0876
wwmairs <wwmairs@gmail.com>
2017-05-22 11:37:02 -0400

    testing new 'public' dir

--- bio.js
+++ bio.js
@@ -1,13 +1,16 @@
-// forecast = $.get("https://api.darksky.net/forecast/b98bd842e0894e2f05cb3bc94579718c/37.8267,-122.4233");
-
-// console.log(forecast);
-
+// client-side javascript for bio page
 
 $(".panel").height($(window).height());
 
 $(document).scroll(function() {
-  if($(window).scrollTop() + $(window).height() == $(document).height()) {
-    $(".navbar").show("blind")
-  }
-  else $(".navbar").hide("blind")
+    if($(window).scrollTop() + $(window).height() == $(document).height()) {
+      $(".navbar").show("blind")
+    }
+    else $(".navbar").hide("blind")
+});
+
+console.log('hello');
+
+$.get('weather.json', function(data) {
+    console.log(data);
 });
