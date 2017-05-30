452b7a6067324c108b4f0c3c4a9301ed73b4838d
wwmairs <wwmairs@gmail.com>
2017-05-30 15:14:29 -0400

    debugging where.html

--- where.js
+++ where.js
@@ -22,8 +22,27 @@ $.get('weather.json', {pass: "sweetboy" }, function(data) {
     $('#where').css('color', color);
 });
 $.get('sun.json', function(data) {
-    // json = JSON.parse(data);
+    console.log(data);
     sunrise = data.sunrise;
     sunset = data.sunset;
-    console.log(sunrise.features[0].properties.quality);
+    rise_quality = sunrise.features[0].properties.quality;
+    set_quality = sunset.features[0].properties.quality;
+    new_p = "<p>";
+    switch (set_quality) {
+        case "Poor" :
+            new_p += "Tonight the sunset isn't supposed to be great.. oh well."
+            break;
+        case "Fair" :
+            new_p += "Tonight's sunset should be OK too."
+            break;
+        case "Good" :
+            new_p += "Tonight the sunset is gonna be good! Neat-o!"
+            break;
+        case "Great" :
+            new_p += "The sunset tonight is supposed to be FAB.  I'll be watching."
+            break;
+
+    }
+    new_p += "</p>";
+    $('content').append(new_p);
 });
