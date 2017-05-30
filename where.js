759a896b3d724821bd358c4205ed80d1774561c1
wwmairs <wwmairs@gmail.com>
2017-05-30 14:49:36 -0400

    separating pages and more colors

--- where.js
+++ where.js
@@ -1,6 +1,5 @@
 // client-side javascript for bio page
 
-$(".pane").height($(window).height());
 
 $(document).scroll(function() {
     if($(window).scrollTop() + $(window).height() == $(document).height()) {
@@ -18,7 +17,7 @@ $.get('weather.json', {pass: "sweetboy" }, function(data) {
     background_color = 'rgba(51, 153, 255, ' + (1 - clouds) + ')';
     color = 'rgba(0, 0, 0, ' + clouds + ')';
 
-    $('#weather').append(temp + ' degrees, and the barometric pressure is ' + pressure + ' milibars.');
+    $('#content').append(temp + ' degrees, and the barometric pressure is ' + pressure + ' milibars.');
     $('#where').css('background', background_color);
     $('#where').css('color', color);
 });
