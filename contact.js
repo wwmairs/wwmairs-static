947929b89d7d46a236d08478d10118fa63cab243
wwmairs <wwmairs@gmail.com>
2017-06-01 16:53:47 -0400

    starting on modals on contact.html

---
+++ contact.js
@@ -0,0 +1,33 @@
+$(document).scroll(function() {
+    if($(window).scrollTop() + $(window).height() == $(document).height()) {
+        $(".nav").show("blind")
+    } else {
+        $(".nav").hide("blind");
+    }
+});
+
+var sent = getUrlParameter('sent');
+var modal = modal = document.getElementById('theModal');
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function (event) {
+    if (even.target == modal) {
+        modal.style.display = "none";
+    }
+}
+if (sent == 'true') {
+    modal.style.display = "block";
+} else if (sent == 'false') {
+    modal.style.display = "block";
+} else {
+
+}
+
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
