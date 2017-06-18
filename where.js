8c208016364395690fe5775d02875e81eb77271c
wwmairs <wwmairs@gmail.com>
2017-06-18 17:38:56 -0400

    debugging integration of p5 and normal js

--- where.js
+++ where.js
@@ -1,46 +1,47 @@
-$.get('weather.json', {pass: "sweetboy" }, function(data) {
-    forecast = JSON.parse(data);
-    temp = forecast.currently.apparentTemperature;
-    pressure = forecast.currently.pressure;
-    clouds = forecast.currently.cloudCover;
-    background_color = 'rgba(51, 153, 255, ' + (1 - clouds) + ')';
-    riseDate = new Date(forecast.daily.data[0].sunriseTime * 1000);
-    setDate  = new Date(forecast.daily.data[0].sunsetTime * 1000);
-    sessionStorage.setItem('riseTime', riseDate.getHours());
-    sesstionStorage.setItem('setTime', setDate.getHours());
-
-    $('#weather').append(temp + ' degrees, and the barometric pressure is ' + pressure + ' milibars.');
-
-    // $('body').css('background', background_color);
-});
-
-$.get('sun.json', function(data) {
-    sunrise = data.sunrise;
-    sunset = data.sunset;
-    rise_quality = sunrise.features[0].properties.quality;
-    set_quality = sunset.features[0].properties.quality;
-    switch (set_quality) {
-        case "Poor" :
-            $('#sunset').append("Often there are beautiful sunsets here, but not tonight.");
-            break;
-        case "Fair" :
-            $('#sunset').append("The sunset here is supposed to be OK tonight.");
-            break;
-        case "Good" :
-            $('#sunset').append("Tonight the sunset is gonna be a good one! Neat-o!");
-            break;
-        case "Great" :
-            $('#sunset').append("The sunset tonight is supposed to be FAB.  I'll be watching.");
-            break;
-        default :
-            $('#sunset').append("Whoops, there's a bug in my sunset predictor;");
-
-    }
-});
-
-// p5.js ish
-
 new p5(); 
+
+preload(){
+
+    $.get('weather.json', {pass: "sweetboy" }, function(data) {
+        forecast = JSON.parse(data);
+        temp = forecast.currently.apparentTemperature;
+        pressure = forecast.currently.pressure;
+        clouds = forecast.currently.cloudCover;
+        background_color = 'rgba(51, 153, 255, ' + (1 - clouds) + ')';
+        riseDate = new Date(forecast.daily.data[0].sunriseTime * 1000);
+        setDate  = new Date(forecast.daily.data[0].sunsetTime * 1000);
+        sessionStorage.setItem('riseTime', riseDate.getHours());
+        sessionStorage.setItem('setTime', setDate.getHours());
+
+        $('#weather').append(temp + ' degrees, and the barometric pressure is ' + pressure + ' milibars.');
+
+        // $('body').css('background', background_color);
+    });
+
+    $.get('sun.json', function(data) {
+        sunrise = data.sunrise;
+        sunset = data.sunset;
+        rise_quality = sunrise.features[0].properties.quality;
+        set_quality = sunset.features[0].properties.quality;
+        switch (set_quality) {
+            case "Poor" :
+                $('#sunset').append("Often there are beautiful sunsets here, but not tonight.");
+                break;
+            case "Fair" :
+                $('#sunset').append("The sunset here is supposed to be OK tonight.");
+                break;
+            case "Good" :
+                $('#sunset').append("Tonight the sunset is gonna be a good one! Neat-o!");
+                break;
+            case "Great" :
+                $('#sunset').append("The sunset tonight is supposed to be FAB.  I'll be watching.");
+                break;
+            default :
+                $('#sunset').append("Whoops, there's a bug in my sunset predictor;");
+
+        }
+    });
+}
 
 function setup() {
     const HEIGHT = windowHeight;
