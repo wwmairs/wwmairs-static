ada8a07dc8a4748058502e1565104124d79bf393
wwmairs <wwmairs@gmail.com>
2017-07-27 10:06:12 -0400

    debuggin where

--- where.js
+++ where.js
@@ -13,7 +13,7 @@ function preload(){
     WIDTH = windowWidth;
 
 // TODO: change this back to just weather.json !!!
-    $.get('192.168.0.15/weather.json', {pass: "sweetboy" }, function(data) {
+    $.get('/weather.json', {pass: "sweetboy" }, function(data) {
         forecast = JSON.parse(data);
         temp = forecast.currently.apparentTemperature;
         pressure = forecast.currently.pressure;
@@ -34,7 +34,7 @@ function preload(){
     console.log(riseTime);
 
 // TODO: change this back to just sun.json !!!
-    $.get('192.168.0.15/sun.json', function(data) {
+    $.get('/sun.json', function(data) {
         sunrise = data.sunrise;
         sunset = data.sunset;
         rise_quality = sunrise.features[0].properties.quality;
