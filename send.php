0f2d94f0f4405e55050d32a89b5e50bfceb671f2
wwmairs <wwmairs@gmail.com>
2017-08-29 11:47:45 -0400

    debugging

--- send.php
+++ send.php
@@ -3,7 +3,7 @@
     $name = $_GET['name'];
     $email = $_GET['email'];
     $message = $_GET['message'];
-    $message = wordwrap($message, 70, "\r\n");
+    // $message = wordwrap($message, 70, "\r\n");
     echo "<h1>" . $message . "</h1>";
     // $success = mail($dest, "from: " . $email, $message);
     // echo "<h2" . $success . "</h2>";
