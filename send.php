d985b741890189203b1f3e0a973f0fc6912154b0
wwmairs <wwmairs@gmail.com>
2017-08-30 15:53:02 -0400

    installed sendmail

--- send.php
+++ send.php
@@ -3,9 +3,15 @@
     $name = $_GET['name'];
     $email = $_GET['email'];
     $message = $_GET['message'];
-    $success = TRUE;
+    $success = False;
     $message = wordwrap($message, 70, "\r\n");
     echo "<h1>" . $message . "</h1>";
-    mail($dest, "from: " . $email, $message);
-    echo "<h2" . $success . "</h2>";
+    $success = mail($dest, "from: " . $email, $message);
+    if ($success) {
+        echo "<h2> success </h2>";
+    }
+    else {
+        echo "<h2> not success </h2>";
+    }
+    
 ?>
