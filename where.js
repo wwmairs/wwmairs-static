f054130cc4a1f925f1cfaa64fc1a2752ca7a070e
wwmairs <wwmairs@gmail.com>
2018-03-27 10:55:04 -0400

    changed gets in where.js to ips pointing to praline

--- where.js
+++ where.js
@@ -32,8 +32,7 @@ function preload(){
     HEIGHT = windowHeight;
     WIDTH = windowWidth;
 
-// TODO: change this back to just weather.json !!!
-    $.get('/api/weather.json', {pass: "sweetboy" }, function(data) {
+    $.get('209.6.43.162/weather.json', {pass: "sweetboy" }, function(data) {
         forecast = JSON.parse(data);
         temp = forecast.currently.apparentTemperature;
         pressure = forecast.currently.pressure;
@@ -53,8 +52,7 @@ function preload(){
     });
     console.log(riseTime);
 
-// TODO: change this back to just sun.json !!!
-    $.get('/api/sun.json', function(data) {
+    $.get('209.6.43.162/sun.json', function(data) {
         sunrise = data.sunrise;
         sunset = data.sunset;
         rise_quality = sunrise.features[0].properties.quality;
