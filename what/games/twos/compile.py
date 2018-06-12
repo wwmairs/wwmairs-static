a8c0e9e3e1584e815b42c97046b32cb61d0c068e
wwmairs <wwmairs@gmail.com>
2018-06-12 13:02:12 -0300

    working twos csv compiler

--- what/games/twos/compile.py
+++ what/games/twos/compile.py
@@ -3,20 +3,26 @@
 # and rankings in following rows
 
 import sys
+import json
 
 with open(sys.argv[1], 'r') as readfile:
 	players = []
-	names = readfile[0].split(',')
+	firstline = readfile.readline().strip()
+	names = firstline.split(',')
 	num_players = len(names)
-  for name in names:
-		players.append({'name': name, 'scores': [num_players]})	
+	for name in names:
+		players.append({'name': name, 'scores': [0 for x in range(num_players)]})	
 	for line in readfile:
 		places = line.split(',')
-		# add to scores array for respective player
-		# scores is an array where the first element 
-		# is the number of first places,
-		# the second the number of second places ...
+		for i, p in enumerate(places):
+			# add to scores array for respective player
+			# scores is an array where the first element 
+			# is the number of first places,
+			# the second the number of second places ...
+			place = int(p)
+			players[i]["scores"][place - 1] += 1
 		
 
 with open(sys.argv[2], 'w') as writefile:
+	writefile.write(json.dumps(players))
 
